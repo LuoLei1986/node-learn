b5bb9d962df58bc489b5dc069c909ffe797470a2
luolei1986 <j-luolei@msn.com>
2020-06-30 09:36:07 +0800

    add about the koa2

---
+++ http/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var http = require('http');
+
+var server =  http.createServer(function(req, res){
+    console.log(req.method  + ':' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello world!</h1>')
+})
+
+server.listen(8000);
+console.log('serve is running at http://127.0.0.1:8000');
